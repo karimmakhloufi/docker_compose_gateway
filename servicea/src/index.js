ee38a64303dfc707516cbd92446cda4889798ae7
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-07-20 14:06:37 +0200

    Added nodemon in services and volumes in docker compose

--- servicea/src/index.js
+++ servicea/src/index.js
@@ -6,6 +6,10 @@ app.get("/hello", (req, res) => {
   res.send("Hello from service a!");
 });
 
+app.get("/testservicea", (req, res) => {
+  res.send("Hello from test service a!");
+});
+
 app.listen(port, () => {
   console.log(`Server is running`);
 });
